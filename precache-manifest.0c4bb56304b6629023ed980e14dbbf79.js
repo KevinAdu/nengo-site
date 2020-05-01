5cc3a9682177cc91aa8dbdcc14f2ff80e72dd2e3
KevinAdu <kevyugi@gmail.com>
2020-05-01 22:59:52 +0900

    Updates

---
+++ precache-manifest.0c4bb56304b6629023ed980e14dbbf79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f29771472a725380e9fe8706a0ca4515",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da5f4775f2e3c5ef26e8",
+    "url": "/static/css/main.19aa7ad1.chunk.css"
+  },
+  {
+    "revision": "49f4f1ac3680d28a2ca6",
+    "url": "/static/js/2.fd0af77a.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.fd0af77a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da5f4775f2e3c5ef26e8",
+    "url": "/static/js/main.d1ecb754.chunk.js"
+  },
+  {
+    "revision": "2bb0a43910d7f77a53e2",
+    "url": "/static/js/runtime-main.5db51f94.js"
+  }
+]);
